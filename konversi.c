0e89de55b8c6a88e27bee3ed435bba65f90d576b
mandenasneli <neliagustinamandenas24@gmail.com>
2024-12-09 21:03:46 +0900

    tadi ada kendala

---
+++ konversi.c
@@ -0,0 +1,22 @@
+
+
+void dolar()
+
+{
+
+   float rupiah, dolar;
+
+   printf("Masukan Jumlah rupiah : ");
+   scanf("%f",&rupiah);
+   dolar = rupiah/15943;
+   printf ("\t nilai konversi rupiah ke dolar adalah : $ %f\n", dolar);
+}
+
+void rupiah()
+{
+   float rupiah, dolar;
+   printf("Masukan Jumlah dolar : ");
+   scanf("%f",&dolar);
+   rupiah = dolar*15943;
+   printf ("\t nilai konversi rupiah ke dolar adalah  : Rp %f\n", rupiah);
+}
---
+++ main.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include "nell.h"
+
+int main ()
+{
+  printf("\t\t Library sistem operasi");
+  printf("============================= \n");
+  printf("Welcome to converter money");
+  printf("============================= \n");
+  printf("\n nilai dolar ke rupiah \n");
+  rupiah();
+  
+   printf("\n nilai rupiah ke dolar \n");
+   dolar();
+
+}
